80bba5ab9c2f6e0d56187397059b886c7bd33c5d
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-12-06 16:50:18 -0500

    Initial commit

---
+++ be-preactive.js
@@ -0,0 +1,33 @@
+// @ts-check
+import { BE } from 'be-enhanced/BE.js';
+import { propInfo, resolved, rejected } from 'be-enhanced/cc.js';
+import { render } from 'preact';
+import { html } from 'htm/preact';
+import {dispatchEvent as de} from 'trans-render/positractions/dispatchEvent.js';
+import {BeRenderNeutral} from 'be-render-neutral/be-render-neutral.js';
+
+/** @import {BEConfig, IEnhancement, BEAllProps} from './ts-refs/be-enhanced/types' */
+/** @import {Actions, PAP,  AP, BAP} from './ts-refs/be-render-neutral/types' */;
+/** @import {Specifier} from './ts-refs/trans-render/dss/types' */
+
+/**
+ * @implements {Actions}
+ * 
+ */
+class BePreactive extends BeRenderNeutral {
+
+    /**
+     * 
+     * @param {BAP} self 
+     */
+    doRender(self) {
+        const {renderer, vm, enhancedElement} = self;
+        const {parentElement} = enhancedElement;
+        if(parentElement === null) throw 400;
+        render(renderer(vm, html), parentElement);
+    }
+
+}
+
+await BePreactive.bootUp();
+export {BePreactive}
